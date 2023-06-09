99ac723a619cead40dd1e4edffd217500c1630bc
Trieu Nguyen <trieu.nguyennhu@gmail.com>
2023-06-09 10:09:43 +0700

    Add pagination to gallery page

--- wordpress/wp-content/themes/rosegarden_file/gallery.php
+++ wordpress/wp-content/themes/rosegarden_file/gallery.php
@@ -5,23 +5,24 @@ Template Name: ギャラリーテンプレート
 get_header();
 ?><?php include('header_icon.php'); ?>
 <?php include('cta.php'); ?>
+<?php $galleryPostId = 8189;?>
 
 <!-- FV BGあり -->
 <!-- PC BG -->
 <?php
 // SCF::get_post_meta($post->ID, '設定した名前', 画像サイズ)
-$img = get_post_meta($post->ID, 'image_pc', true);
+$img = get_post_meta($galleryPostId, 'image_pc', true);
 ?>
 <div class="page-background" style="background-image:linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(<?php echo wp_get_attachment_url($img) ?>)">
     <div class="page-title_05">
         <div class="page-title_05__eng">
-            <p><?php echo SCF::get('title_en'); ?></p>
+            <p><?php echo SCF::get('title_en', $galleryPostId); ?></p>
         </div>
         <div class="page-title_05__jp">
-            <p><?= get_the_title() ?></p>
+            <p><?= get_the_title($galleryPostId) ?></p>
         </div>
         <div class="page-title_05__contents">
-            <p><?php echo SCF::get('fv_text'); ?></p>
+            <p><?php echo SCF::get('fv_text', $galleryPostId); ?></p>
         </div>
     </div>
 </div>
@@ -31,51 +32,34 @@ $img = get_post_meta($post->ID, 'image_pc', true);
 
 <?php
 // SCF::get_post_meta($post->ID, '設定した名前', 画像サイズ)
-$img = get_post_meta($post->ID, 'image_sp', true);
+$img = get_post_meta($galleryPostId, 'image_sp', true);
 ?>
 <!-- SP BG -->
 <div class="page-background_sp" style="background-image:linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(<?php echo wp_get_attachment_url($img) ?>)">
-    <img src="<?php the_field('image_pc'); ?>" alt="">
+    <img src="<?php the_field('image_pc', $galleryPostId); ?>" alt="">
     <div class="page-title_05">
         <div class="page-title_05__eng">
-            <p><?php echo SCF::get('title_en'); ?></p>
+            <p><?php echo SCF::get('title_en', $galleryPostId); ?></p>
         </div>
         <div class="page-title_05__jp">
-            <p><?= get_the_title() ?></p>
+            <p><?= get_the_title($galleryPostId) ?></p>
         </div>
         <div class="page-title_05__contents">
-            <p><?php echo SCF::get('fv_text'); ?></p>
+            <p><?php echo SCF::get('fv_text', $galleryPostId); ?></p>
         </div>
     </div>
 </div>
 
+<?php
+$current_category = get_queried_object();
+$category_slug = '';
+$category_id = '';
 
-
-
-<!-- js -->
-<script>
-    $(document).ready(function() {
-        $('.ctg_btn__child-btn').click(function() {
-            // すべてのボタンからactiveクラスを削除
-            $('.ctg_btn__child-btn').removeClass('active');
-            // クリックされたボタンにactiveクラスを追加
-            $(this).addClass('active');
-
-            // クリックされたボタンのデータカテゴリを取得
-            var category = $(this).data('category');
-
-            // 投稿をフィルタリング
-            $('.gallery__photo__item').each(function() {
-                var postCategories = $(this).data('categories');
-                if (category === 'all' || postCategories.includes(category)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
-        });
-    });
-</script>
+if ($current_category instanceof WP_Term) {
+    $category_slug = $current_category->slug;
+    $category_id = $current_category->term_id;
+}
+?>
 
 <!-- ギャラリー -->
 <!-- カテゴリボタン -->
@@ -83,19 +67,22 @@ $img = get_post_meta($post->ID, 'image_sp', true);
     <div class="church__ctg">
         <div class="ctg_btn">
             <div class="ctg_btn__all">
-                <button class="ctg_btn__child-btn active" data-category="all">
-                    <p>ALL</p>
-                </button>
+                <a href="<?php echo the_permalink($galleryPostId);?>">
+                    <button class="ctg_btn__child-btn <?php echo (empty($category_id)) ? 'active' : '';?>" data-category="all">
+                        <p>ALL</p>
+                    </button>
+                </a>
             </div>
             <div class="ctg_btn__child-wrap">
                 <?php
                 $taxonomy_name = "gallery-cat"; // カテゴリータクソノミーのスラッグを指定してください
                 $terms = get_terms($taxonomy_name);
-                foreach ($terms as $term) {
-                    $term_link = get_term_link($term);
-                    echo '<button class="ctg_btn__child-btn" data-category="' . esc_attr($term->slug) . '">' . $term->name . '</button>';
-                }
+                foreach ($terms as $term) :
                 ?>
+                <a href="<?php echo get_term_link($term, $taxonomy_name)?>">
+                <button class="ctg_btn__child-btn <?php echo ($category_id == $term->term_id) ? 'active' : '';?>" data-category="<?php echo esc_attr($term->slug);?>"><?php echo $term->name;?></button>
+                </a>
+                <?php endforeach;?>
             </div>
         </div>
     </div>
@@ -111,11 +98,28 @@ $img = get_post_meta($post->ID, 'image_sp', true);
     <!-- ギャラリー 写真-->
     <div class="gallery__photo">
         <?php
+        $posts_per_page = 18;
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        if (!empty($category_slug)) {
+            $paged = (get_query_var('page')) ? get_query_var('page') : 1;
+        }
+        $post_type = 'gallery';
         $args = array(
-            'posts_per_page' => -1,
+            'posts_per_page' => $posts_per_page,
+            'paged' => $paged,
             'post_status' => 'publish',
-            'post_type' => 'gallery',
+            'post_type' => $post_type,
         );
+
+        if (!empty($category_slug)) {
+            $args['tax_query'] = array(
+                array(
+                    'taxonomy' => $taxonomy_name,
+                    'field' => 'slug',
+                    'terms' => $category_slug,
+                ),
+            );
+        }
 
         $myposts = get_posts($args);
         foreach ($myposts as $post) : setup_postdata($post);
@@ -126,6 +130,25 @@ $img = get_post_meta($post->ID, 'image_sp', true);
         endforeach;
         wp_reset_postdata();
         ?>
+
+        <!-- ページネーション -->
+        <div style="width: 100%;">
+        <?php 
+        if (empty($category_slug)) {
+            $total_post = wp_count_posts($post_type)->publish;
+        } else {
+            $total_post = get_term($category_id)->count;
+        }
+        
+        $num_pages = ceil($total_post / $posts_per_page);
+        
+        $args = array(
+            'total' => $num_pages,
+            'found_posts' => $total_post
+        );
+        echo custom_pagination($args); 
+        ?>
+        </div>
     </div>
 </div>
 
